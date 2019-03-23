678b980bf1b349e612e3ef74fab85b34e6a09b9c
cyrilHCD <lang.cyril.pro@gmail.com>
2019-03-23 14:24:09 +0100

    ajout de l'agréabilité de chaque zone

--- model.py
+++ model.py
@@ -32,6 +32,7 @@ class Zone:
     MAX_LATITUDE_DEG = 90
     WIDTH_DEG = 1
     HEIGHT_DEG = 1
+    EARTH_RADIUS_KILOMETERS = 6371
     
     def __init__(self, corner_left, corner_right):
         self.corner_left = corner_left
@@ -55,8 +56,23 @@ class Zone:
     def population(self):
         return len(self.inhabitants)
 
+    @property
+    def width(self):
+        return (abs(self.corner_left.longitude - self.corner_right.longitude)
+                * self.EARTH_RADIUS_KILOMETERS)
+    @property
+    def height(self):
+        return (abs(self.corner_left.latitude - self.corner_right.latitude)
+                * self.EARTH_RADIUS_KILOMETERS)
+    @property
+    def area(self):
+        return self.height * self.width
+    
+    def population_density(self):
+        return self.population / self.area
+
     @classmethod
-    def initialize_zones(cls):
+    def _initialize_zones(cls):
         for latitude in range(cls.MIN_LATITUDE_DEG,
                               cls.MAX_LATITUDE_DEG,
                               cls.HEIGHT_DEG):
@@ -71,6 +87,8 @@ class Zone:
 
     @classmethod
     def find_zone_that_contains(cls, position):
+        if not cls.ZONES:
+            cls._initialize_zones()
         # Compute the index in the ZONES array that contains the given position
         longitude_index = int((position.longitude_deg
                                - cls.MIN_LONGITUDE_DEG)
@@ -88,10 +106,13 @@ class Zone:
         assert zone.contains(position)
 
         return zone
-        
+
+    def average_agreeableness(self):
+         if not self.population:
+             return 0
+         return sum([inhabitant.agreeableness for inhabitant in self.inhabitants]) / self.population
         
 def main():
-    Zone.initialize_zones()
     for agents_attributes in json.load(open("agents-100k.json")):
         latitude = agents_attributes.pop('latitude')
         longitude = agents_attributes.pop('longitude')
@@ -99,6 +120,6 @@ def main():
         agent = Agent(position, **agents_attributes)
         zone = Zone.find_zone_that_contains(position)
         zone.add_inhabitants(agent)
-        print("Zone population: ", zone.population)
+        print(zone.average_agreeableness())
 main()
 
